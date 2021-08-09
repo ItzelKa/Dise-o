d5fb80922e5dceb9166969d97d5c0caeb0b8efe5
Alexander <85037538+Alexander-R-S@users.noreply.github.com>
2021-08-09 17:25:47 -0500

    diseño e implementacion del glosario y vistas de las dos especies de los madoños las cuales contienen info asi como la creacion del codigo backen del nuevo fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,51 +1,136 @@
 $(document).ready(function() {
-    
+
+	for (var i = 1; i <=1; i++) {
+		arbutusConsulta(i);
+	}
+
     function arbutusConsulta(dato) {
-		fetch('http://localhost:3000/arbutus', {
-            method: 'GET',
-		}).then(res => res.json())
+		fetch('http://localhost:3000/arbutus',{method: 'GET'})
+		.then(res => res.json())
 		.then(function(data) {
 			var cont="";
+			var img="";
 			console.log(data);
-			//alert("post arbutus", data.arbutus.length);
 			data.arbutus.forEach(function(dato){ 
-				cont+='<p>'+dato.nombre+'</p>';	
-			});
-			document.getElementById("divArbutus").innerHTML=cont;
-			
+				cont=dato.nombre;
+				img=dato.imagenes.imagen1;
+				document.getElementById('divArbutus').innerHTML+=`
+				<div class="col-6" data-aos="fade-up">
+					<div class="cont-comarosta">
+						<div class="comarosta-details">
+							<h1>${cont}</h1>
+							<ul>
+								<li><strong>Enves: </strong>${dato.enves}</li>
+								<li><strong>Flores: </strong>${dato.flores}</li>
+								<li><strong>Habito: </strong>${dato.habito}</li>
+								<li><strong>Haz: </strong>${dato.haz}</li>
+							</ul>
+						</div>
+						<div class="comarosta-img">
+							<img src="${img}" alt="">
+							<div class="info text-center">
+								<a type="button" data-toggle="modal" data-target="#exampleModalCenter">
+									<h2>click para ver mas</h2>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+					<div class="modal-dialog modal-dialog-centered" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalCenterTitle">${cont}</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+								<ul>
+									<li><strong>Hojas: </strong>${dato.hojas}</li>
+									<li><strong>Corteza ramas: </strong>${dato.corteza_ramas}</li>
+									<li><strong>Corteza ramillas: </strong>${dato.corteza_ramillas}</li>
+									<li><strong>Peciolos: </strong>${dato.peciolos}</li>
+									<li><strong>Ubicacion: </strong>${dato.ubicacion}</li>
+								</ul>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-primary btn-sm" data-dismiss="modal">Cerrar</button>
+							</div>
+						</div>
+					</div>
+				</div>`;
+			});	
+							
 		}).catch(function(error) {
             console.log(error);
 		});
     }
 
-    $('#buscarArbutus').on('click',function(event) {
-        var obj={};
-        arbutusConsulta(obj);
-        //console.log(obj);
-    });
+	for (var i = 1; i <=1; i++) {
+		comarostaConsulta(i);
+	}
 
     function comarostaConsulta(dato) {
-		fetch('http://localhost:3000/comarosta', {
-            method: 'GET',
-		}).then(res => res.json())
+		fetch('http://localhost:3000/comarosta', {method: 'GET'})
+		.then(res => res.json())
 		.then(function(data) {
 			var cont="";
+			var img="";
 			console.log(data);
-			//alert("post arbutus", data.comarosta.length);
 			data.comarosta.forEach(function(dato){ 
-				cont+='<p>'+dato.nombre+'</p>';	
+				cont=dato.nombre;
+				img=dato.imagenes.imagen1;
+				document.getElementById('divComarosta').innerHTML+=`
+				<div class="col-6" data-aos="fade-up">
+					<div class="cont-comarosta">
+						<div class="comarosta-details">
+							<h1>${cont}</h1>
+							<ul>
+								<li><strong>Enves: </strong>${dato.enves}</li>
+								<li><strong>Flores: </strong>${dato.flores}</li>
+								<li><strong>Habito: </strong>${dato.habito}</li>
+								<li><strong>Haz: </strong>${dato.haz}</li>
+							</ul>
+						</div>
+						<div class="comarosta-img">
+							<img src="${img}" alt="">
+							<div class="info text-center">
+								<a type="button" data-toggle="modal" data-target="#exampleModalCenter">
+									<h2>click para ver mas</h2>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+					<div class="modal-dialog modal-dialog-centered" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalCenterTitle">${cont}</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div class="modal-body">
+								<ul>
+									<li><strong>Hojas: </strong>${dato.hojas}</li>
+									<li><strong>Infloresencia: </strong>${dato.inflorescencia}</li>
+									<li><strong>Margen: </strong>${dato.margen}</li>
+									<li><strong>Peciolos ramillas: </strong>${dato.peciolos_ramillas}</li>
+									<li><strong>Ubicacion: </strong>${dato.ubicacion}</li>
+								</ul>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-primary btn-sm" data-dismiss="modal">Cerrar</button>
+							</div>
+						</div>
+					</div>
+				</div>`;
 			});
-			document.getElementById("divComarosta").innerHTML=cont;
-			
 		}).catch(function(error) {
             console.log(error);
 		});
     }
 
-    $('#buscarComarosta').on('click',function(event) {
-        var obj={};
-        comarostaConsulta(obj);
-        //console.log(obj);
-    });
-    
 });
