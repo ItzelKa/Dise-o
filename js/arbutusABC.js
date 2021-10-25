f7885ae07ba467ee9e005a37e5de6919d28b1a55
Alexander <85037538+Alexander-R-S@users.noreply.github.com>
2021-10-25 12:27:43 -0500

    Cambio de diseño de la parte de seleccion de los estados

--- js/arbutusABC.js
+++ js/arbutusABC.js
@@ -1,154 +1,118 @@
-$(document).ready(function () {
-	var vacio = 0;
+var estados = [];
 
-	//Boton limpiar
-	$('#limpia').on('click', function () {
-		$('#formarbu').trigger('reset');
-	});
+///////  FUNCIONES FETCH  /////////
+////////////////////////////////////////////////////////
 
-	//validar campos
-	function validar() {
-		$('input[type=text]').each(function (i, v) {
-			if (this.value.length < 1) {
-				vacio = 1;
-				return false;
-			}
-			vacio = 0;
+///FETCH GET BASICO LISTADO DE ESPECIES INICIAL
+function fetch1() {
+	fetch('http://localhost:3000/arbutus', {
+		method: 'GET',
+	}).then(res => res.json())
+		.then(function (data) {
+			var cont = ""
+			data.arbutus.forEach(function (dato, index) {
+				cont += '<tr><td scope="row">' + (index + 1) + '</td><td>' + dato.nombre + '</td><td><span class="d-inline-flex"><button type="button" class="btn btn-success edit" id="' + dato._id + '" >Editar</button><button type="button" class="btn btn-danger delete" id="' + dato._id + '" value="' + dato.nombre + '">Eliminar</button></span></td></tr>';
+			});
+			document.getElementById("espa").innerHTML = cont;
+		}).catch(function (error) {
+			console.log(error);
 		});
-	}
+}
 
-	///Limitar input file a 3 archivos
-	$("#img").on("change", function () {
-		if ($("#img")[0].files.length > 3) {
-			alert("Solo se pueden seleccionar 3 imagenes como maximo");
-			$("#inserta").prop('disabled', true);
+///FETCH INSERTAR
+function fetchinsert(datos) {
+	fetch('http://localhost:3000/arbutus/add', {
+		method: 'POST',
+		body: datos,
+		headers: {
+			'Content-Type': 'application/json',
+			"Accept": "application/json"
 		}
-		else {
-			$("#inserta").prop('disabled', false);
+	}).then(res => res.json())
+		.then(function (data) {
+			alert(data.message);
+			fetch1();
+			limpia();
+			$('#insert').modal('hide');
+		}).catch(function (error) {
+			console.log(error);
+		});
+}
+
+///FETCH ELIMINAR
+function fetchdelete(c, v) {
+	fetch('http://localhost:3000/arbutus/' + c + '/' + v, {
+		method: 'DELETE'
+	}).then(res => res.json())
+		.then(function (data) {
+			alert(data.message + ': ' + data.arBaja.nombre);
+			fetch1();
+		}).catch(function (error) {
+			console.log(error);
+		});
+}
+
+///FETCH ACTUALIZAR1 TRAER DATOS
+function fetcheditar1(id) {
+	fetch('http://localhost:3000/arbutus/_id/' + id, {
+		method: 'GET',
+	}).then(res => res.json())
+		.then(function (data) {
+			$('#id').val(data.arbutus[0]._id);
+			$('#nom').val(data.arbutus[0].nombre);
+			$('#hab').val(data.arbutus[0].habito);
+			$('#cor1').val(data.arbutus[0].corteza_ramas);
+			$('#cor2').val(data.arbutus[0].corteza_ramillas);
+			$('#pec').val(data.arbutus[0].peciolos);
+			$('#hoj').val(data.arbutus[0].hojas);
+			$('#haz2').val(data.arbutus[0].haz);
+			$('#env').val(data.arbutus[0].enves);
+			$('#flo').val(data.arbutus[0].flores);
+
+			let src1 = data.arbutus[0].imagenes.imagen1;
+			let src2 = data.arbutus[0].imagenes.imagen2;
+			let src3 = data.arbutus[0].imagenes.imagen3;
+
+			if (src1) { $('#foto1').attr('src', src1); }
+			if (src2) { $('#foto2').attr('src', src2); }
+			if (src3) { $('#foto3').attr('src', src3); }
+
+			let est = [];
+			est = data.arbutus[0].ubicacion;
+
+			for (let i = 0; i < est.length; i++) {
+				$("#edit input[value='" + est[i] + "']").attr("checked", true);
+			}
+			$('#edit').modal('show');
+		}).catch(function (error) {
+			console.log(error);
+		});
+}
+
+///FETCH ACTUALIZAR2 GUARDAR CAMBIOS
+function fetcheditar2(id, datos) {
+	fetch('http://localhost:3000/arbutus/_id/' + id, {
+		method: 'PUT',
+		body: datos,
+		headers: {
+			'Content-Type': 'application/json',
+			"Accept": "application/json"
 		}
-	});
+	}).then(res => res.json())
+		.then(function (data) {
+			alert(data.message);
+			fetcheditar1(id);
+		}).catch(function (error) {
+			console.log(error);
+		});
+}
 
-	///FETCH GET
-	function fetch1() {
-		fetch('http://localhost:3000/arbutus', {
-			method: 'GET',
-		}).then(res => res.json())
-			.then(function (data) {
-				var cont = ""
-				data.arbutus.forEach(function (dato, index) {
-					cont += '<tr><td scope="row">' + (index + 1) + '</td><td>' + dato.nombre + '</td><td><span class="d-inline-flex"><button type="button" class="btn btn-success edit" id="' + dato._id + '" >Editar</button><button type="button" class="btn btn-danger delete" id="' + dato._id + '" value="' + dato.nombre + '">Eliminar</button></span></td></tr>';
-				});
+///////	OTRAS FUNCIONES NECESARIAS ////////
+////////////////////////////////////////////////////////
 
-				//var ruta = data.arbutus[0];
-				console.log(data.arbutus);
-				// ruta.ubicacion.forEach(function (dato, index) {
-				// 	cont += dato;
-				// 	console.log(ruta.ubicacion[index]);
-				// });
-				document.getElementById("espa").innerHTML = cont;
-			}).catch(function (error) {
-				console.log(error);
-			});
-	}
-
-	///FETCH INSERT
-	function fetchinsert(datos) {
-
-		fetch('http://localhost:3000/arbutus/add', {
-			method: 'POST',
-			body: datos,
-			headers: {
-				'Content-Type': 'application/json',
-				"Accept": "application/json"
-			}
-		}).then(res => res.json())
-			.then(function (data) {
-				alert(data.message);
-				fetch1();
-				$('#insert').modal('hide');
-			}).catch(function (error) {
-				console.log(error);
-			});
-	}
-
-	///FETCH ACTUALIZAR
-	function fetcheditar1(id) {
-
-		fetch('http://localhost:3000/arbutus/_id/' + id, {
-			method: 'GET',
-		}).then(res => res.json())
-			.then(function (data) {
-				$('#id').val(data.arbutus[0]._id);
-				$('#nom').val(data.arbutus[0].nombre);
-				$('#hab').val(data.arbutus[0].habito);
-				$('#cor1').val(data.arbutus[0].corteza_ramas);
-				$('#cor2').val(data.arbutus[0].corteza_ramillas);
-				$('#pec').val(data.arbutus[0].peciolos);
-				$('#hoj').val(data.arbutus[0].hojas);
-				$('#haz2').val(data.arbutus[0].haz);
-				$('#env').val(data.arbutus[0].enves);
-				$('#flo').val(data.arbutus[0].flores);
-
-				let src1 = data.arbutus[0].imagenes.imagen1;
-				let src2 = data.arbutus[0].imagenes.imagen2;
-				let src3 = data.arbutus[0].imagenes.imagen3;
-
-				if (src1) { $('#foto1').attr('src', src1); }
-				if (src2) { $('#foto2').attr('src', src2); }
-				if (src3) { $('#foto3').attr('src', src3); }
-
-				let estados = [];
-				estados = data.arbutus[0].ubicacion;
-
-				for (let i = 0; i < estados.length; i++) {
-					$("#ubi option[value='" + estados[i] + "']").attr("selected", true);
-				}
-
-				$('#edit').modal('show');
-			}).catch(function (error) {
-				console.log(error);
-			});
-	}
-
-	///FETCH EDITAR2 
-	function fetcheditar2(id, datos) {
-
-		fetch('http://localhost:3000/arbutus/_id/' + id, {
-			method: 'PUT',
-			body: datos,
-			headers: {
-				'Content-Type': 'application/json',
-				"Accept": "application/json"
-			}
-		}).then(res => res.json())
-			.then(function (data) {
-				alert(data.message);
-				fetcheditar1(id);
-			}).catch(function (error) {
-				console.log(error);
-			});
-	}
-
-	///FETCH ELIMINAR
-	function fetchdelete(c, v) {
-		fetch('http://localhost:3000/arbutus/' + c + '/' + v, {
-			method: 'DELETE'
-		}).then(res => res.json())
-			.then(function (data) {
-				alert(data.message + ': ' + data.arBaja.nombre);
-				fetch1();
-			}).catch(function (error) {
-				console.log(error);
-			});
-	}
-
-	fetch1();
-
-
-	//FUNCION CONVERTIR IMGS A BASE64
-
-	function base64(file) {
-		if(file!=0){
+//FUNCION CONVERTIR IMGS A BASE64
+function base64(file) {
+	if (file != 0) {
 		return new Promise(function (resolve, reject) {
 			let fr = new FileReader();
 
@@ -160,108 +124,170 @@ $(document).ready(function () {
 			};
 			fr.readAsDataURL(file);
 		});
-		}else{
-			return file;
-		}
+	} else {
+		return file;
+	}
+}
+
+//FUNCION LIMPIAR FORMULARIO, CHECKBOXES Y VARIABLE ESTADOS
+function limpia() {
+	$('#formarbu').trigger('reset');
+	$("#insert input[type=checkbox]").prop('checked', false);
+}
+
+///FUNCION OBTENER VALORES DE CHECKBOX ESTADOS INSERT
+function arrayestados(opt) {
+	estados = [];
+	if (opt == 2) {
+		$("#edit input[type=checkbox]:checked").each(function () {
+			estados.push(this.value);
+		});
+	} else {
+		$("#insert input[type=checkbox]:checked").each(function () {
+			estados.push(this.value);
+		});
+	}
+	if (estados.length == 0) {
+		alert("Selecciona almenos un estado");
+		return true;
+	}
+	return false;
+}
+
+///FUNCION Limitar input file a 3 archivos
+$("#img").on("change", function () {
+	if ($("#img")[0].files.length > 3) {
+		alert("Solo se pueden seleccionar 3 imagenes como maximo");
+		$("#inserta").prop('disabled', true);
+	}
+	else {
+		$("#inserta").prop('disabled', false);
+	}
+});
+
+
+///////	EVENTOS DE BOTONES ////////
+////////////////////////////////////////////////////////
+
+///LISTAR ESPECIES AL CARGAR PAGINA
+$(document).ready(function () {
+	fetch1();
+});
+
+//Boton Actualizar2 guardar cambios
+$('#editar').click(function () {
+	let reader2 = [];
+	let foto1 = document.getElementById('img1').files;
+	let foto2 = document.getElementById('img2').files;
+	let foto3 = document.getElementById('img3').files;
+
+	if (arrayestados(2)) { return false; }
+
+	reader2[0] = foto1[0];
+	reader2[1] = foto2[0];
+	reader2[2] = foto3[0];
+
+	if (foto1.length == 0) { reader2[0] = 0; }
+	if (foto2.length == 0) { reader2[1] = 0; }
+	if (foto3.length == 0) { reader2[2] = 0; }
+
+	reader2[0] = base64(reader2[0]);
+	reader2[1] = base64(reader2[1]);
+	reader2[2] = base64(reader2[2]);
+
+	Promise.all(reader2).then((values) => {
+		let dato = new FormData(document.getElementById('formarbu2'));
+		dato.append('ubicacion', estados);
+		var img1 = values[0];
+		var img2 = values[1];
+		var img3 = values[2];
+
+		if (img1 == 0) { img1 = $('#foto1').attr('src'); }
+		if (img2 == 0) { img2 = $('#foto2').attr('src'); }
+		if (img3 == 0) { img3 = $('#foto3').attr('src'); }
+
+		dato.append('img1', img1);
+		dato.append('img2', img2);
+		dato.append('img3', img3);
+
+		const entradas = Object.fromEntries(dato.entries());
+		const datos = JSON.stringify(entradas);
+
+		var id = $('#id').val();
+
+		fetcheditar2(id, datos);
+	});
+});
+
+//Boton Actualizar1 mostrar modal con datos
+$('body').on('click', '.edit', function () {
+	var id = $(this).prop('id');
+	$("#edit :checkbox").attr("checked", false);
+	fetcheditar1(id);
+});
+
+///BOTON INSERTAR
+$('#inserta').click(function () {
+
+	let imgs = document.getElementById('img').files;
+	let readers = [];
+
+	if (arrayestados()) { return false; }
+
+	for (let i = 0; i < imgs.length; i++) {
+		readers.push(base64(imgs[i]));
 	}
 
+	Promise.all(readers).then((values) => {
+		let img1 = "", img2 = "", img3 = "";
+		let dato = new FormData(document.getElementById('formarbu'));
+		dato.append('ubicacion', estados);
+		img1 = values[0];
+		img2 = values[1];
+		img3 = values[2];
 
-	//Boton Insertar
-	$('#inserta').click(function () {
+		if (img1 === undefined) { img1 = ""; }
+		if (img2 === undefined) { img2 = ""; }
+		if (img3 === undefined) { img3 = ""; }
 
-		var imgs = document.getElementById('img').files;
-		let readers = [];
+		dato.append('img1', img1); dato.append('img2', img2);
+		dato.append('img3', img3);
 
-		for (let i = 0; i < imgs.length; i++) {
-			readers.push(base64(imgs[i]));
-		}
+		const entradas = Object.fromEntries(dato.entries());
+		const datos = JSON.stringify(entradas);
 
-		Promise.all(readers).then((values) => {
-			let img1 = "", img2 = "", img3 = "";
-			let dato = new FormData(document.getElementById('formarbu'));
-			var estados = $('#ubicacion').val();
-			dato.append('ubicacion', estados);
-			img1 = values[0];
-			img2 = values[1];
-			img3 = values[2];
-
-			if (img1 === undefined) { img1 = ""; }
-			if (img2 === undefined) { img2 = ""; }
-			if (img3 === undefined) { img3 = ""; }
-
-			dato.append('img1', img1);
-			dato.append('img2', img2);
-			dato.append('img3', img3);
-
-			const entradas = Object.fromEntries(dato.entries());
-			const datos = JSON.stringify(entradas);
-
-			fetchinsert(datos);
-		});
+		fetchinsert(datos);
 	});
+});
 
-	//Boton Actualizar mostrar modal con datos
-	$('body').on('click', '.edit', function () {
-		var id = $(this).prop('id');
-		fetcheditar1(id);
-	});
+//BOTON ELIMINAR
+$('body').on('click', '.delete', function () {
+	var id = $(this).prop('id');
+	var nombre = $(this).prop('value');
+	var campo = "_id";
+	var opcion = confirm("Esta seguro de eliminar: " + nombre);
+	if (opcion) {
+		fetchdelete(campo, id);
+	}
+});
 
-	//Boton Actualizar registro
-	$('#editar').click(function () {
+///BOTON LIMPIAR FORMULARIO
+$('#limpia').on('click', function () {
+	limpia();
+});
 
-		let reader2 = [];
-		var foto1 = document.getElementById('img1').files;
-		var foto2 = document.getElementById('img2').files;
-		var foto3 = document.getElementById('img3').files;
 
-		reader2[0]=foto1[0];
-		reader2[1]=foto2[0];
-		reader2[2]=foto3[0];
 
-		if (foto1.length == 0) {reader2[0]=0; }
-		if (foto2.length == 0) { reader2[1]=0; }
-		if (foto3.length == 0) { reader2[2]=0; }
-
-		reader2[0]=base64(reader2[0]);
-		reader2[1]=base64(reader2[1]);
-		reader2[2]=base64(reader2[2]);
-
-		Promise.all(reader2).then((values) => {
-			let dato = new FormData(document.getElementById('formarbu2'));
-			var estados = $('#ubi').val();
-			dato.append('ubicacion', estados);
-			var img1=values[0];
-			var img2=values[1];
-			var img3=values[2];
-
-			if(img1==0){img1 = $('#foto1').attr('src');}
-			if(img2==0){img2 = $('#foto2').attr('src');}
-			if(img3==0){img3 = $('#foto3').attr('src');}
-
-			dato.append('img1', img1);
-			dato.append('img2', img2);
-			dato.append('img3', img3);
-
-			const entradas = Object.fromEntries(dato.entries());
-			const datos = JSON.stringify(entradas);
-
-			var id = $('#id').val();
-
-			fetcheditar2(id, datos);
-		});
-	});
-
-	//Boton Eliminar
-	$('body').on('click', '.delete', function () {
-		var id = $(this).prop('id');
-		var nombre = $(this).prop('value');
-		var campo = "_id";
-		var opcion = confirm("Esta seguro de eliminar: " + nombre);
-		if (opcion) {
-			fetchdelete(campo, id);
-		}
-	});
-
+//validar campos
+	// function validar() {
+	// 	$('input[type=text]').each(function (i, v) {
+	// 		if (this.value.length < 1) {
+	// 			vacio = 1;
+	// 			return false;
+	// 		}
+	// 		vacio = 0;
+	// 	});
+	// }
 
 	// function base64(file, callback) {
 	// 	var reader = new FileReader();
@@ -303,5 +329,3 @@ $(document).ready(function () {
 	//for (var entrie of data.entries()) {
 	// 		console.log(entrie[0]+ ': ' + entrie[1]); 
 	// }
-
-});
